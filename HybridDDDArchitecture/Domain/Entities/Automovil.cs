d8f5fed4f1599fd1f2c14801bc24a123bcd3722d
valentin-alfaro <valentinalfaro51@gmail.com>
2025-09-17 20:36:00 -0300

    Commit N°2
    Creación de la entidad Automóvil, con su Validator

---
+++ HybridDDDArchitecture/Domain/Entities/Automovil.cs
@@ -0,0 +1,23 @@
+﻿using Core.Domain.Entities;
+using Domain.Validators;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Automovil : DomainEntity<string, AutomovilEntityValidator>
+    {
+        public int IdAuto { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año_Fabrcacion { get; set; }
+        public int Numero_Motor { get; set; }
+        public int Numero_Chasis { get; set; }
+    }
+}
